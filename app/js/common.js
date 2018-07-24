fadb994a67ad0c6644512f8a4f5a4743524692de
Guk Dmitry <gukdmitry.s@gmail.com>
2018-07-24 22:37:50 +0300

    js for menu

--- app/js/common.js
+++ app/js/common.js
@@ -1,23 +1,39 @@
 $(function() {
 	//$('.carousel').carousel();
+	var scrollerActive = false;
 	function heightDetect() {
 		$(".tutti-top").css("height", $(window).height());
 	}
 	heightDetect();
-			$(window).resize(function() {
-	heightDetect();
+	ScrollerDetect();
+
+	$(window).resize(function() {
+		heightDetect();
 	});
 
 	var header = $(".menu"); // Меню
 	var scrollPrev = 0 // Предыдущее значение скролла
 	
-	$(window).scroll(function() {
-		var scrolled = $(window).scrollTop(); // Высота скролла в px
-		var firstScrollUp = false; // Параметр начала сколла вверх
-		var firstScrollDown = false; // Параметр начала сколла вниз
-		
+	$(document).scroll(function() {
+		ScrollerDetect();
 		// Если скроллим
 	});
 	 
+	function ScrollerDetect() {
+		var s_top = $(window).scrollTop();
+		var yes = $("#mscroller").offset().top;
+		s_top = s_top + 210;
+		if(s_top > yes && scrollerActive==false){
+			scrollerActive = true;
+			$(".menu").addClass("menu-scroller");
+			$("#Deskw_1").addClass("menu-scroller-logo");
+			$("#Deskw_1").parent().addClass("logo-scroller");	
+		} else if(s_top < yes && scrollerActive==true) {
+			scrollerActive = false;
+			$(".menu").removeClass("menu-scroller");
+			$("#Deskw_1").removeClass("menu-scroller-logo");
+			$("#Deskw_1").parent().removeClass("logo-scroller");
+		}
+	}
 });
 
